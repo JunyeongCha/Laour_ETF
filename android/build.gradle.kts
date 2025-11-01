e838647acd251438696e3986671a1f0e034802ca
Junyeong Cha <vokdol@yonsei.ac.kr>
2025-11-02 00:17:22 +0900

    아직도. 제미나이 돌려둔거부터 수정

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+// lib/android/build.gradle.kts
+
+// (★핵심 수정★) 이 buildscript 블록이 누락되었습니다.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // (★핵심 수정★) 구글 서비스 플러그인 자체를 추가합니다.
+        classpath("com.google.gms:google-services:4.4.2") // (안전한 최신 버전)
+    }
+}
+// (★여기까지 추가★)
+
 allprojects {
     repositories {
         google()
